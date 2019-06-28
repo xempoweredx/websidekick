c703556076c66fb071686f5b4cf0fe311de80c2d
xempoweredx <chadgray1@gmail.com>
2019-06-27 20:58:14 -0500

    Update Meta_CPT_Template.php

--- admin/meta/Meta_CPT_Template.php
+++ admin/meta/Meta_CPT_Template.php
@@ -8,6 +8,8 @@ $plural_lower = 'cpts';
 $prefix = 'sidekick_' . $plural_lower . '_';
 $hidden = '_' . $plural_lower . '_hidden_';
 
+$metabox_prefix = '$sidekick_' . $singular_lower . '_meta';
+
 /*******************************************************************************************************
 
 		GENERAL INFO METABOX
@@ -15,7 +17,7 @@ $hidden = '_' . $plural_lower . '_hidden_';
 ********************************************************************************************************/
 
 
-$cn_sermon_meta = new_cmb2_box( array(
+$metabox_prefix = new_cmb2_box( array(
 	'id'            => $prefix . $plural_lower . '_info',
 	'title'         => esc_html__( $singular . ' Information', 'cmb2' ),
 	'object_types'  => array( $plural_lower ), // Post type
@@ -24,7 +26,7 @@ $cn_sermon_meta = new_cmb2_box( array(
 	'__back_compat_meta_box' => true,
 ) );
 
-	$cn_sermon_meta->add_field( array(
+	$metabox_prefix->add_field( array(
 	    'id'				=> $hidden . 'begin',
 	    'type'				=> 'title',
 	    'before_row'		=> 
@@ -32,88 +34,20 @@ $cn_sermon_meta = new_cmb2_box( array(
 			<div class="form-row">',
 	) );
 
-	$cn_sermon_meta->add_field( array(
-		'name'			=> esc_html__( 'FB Video ID', 'cmb2' ),
-		'id'    		=> $prefix . 'fb_vid_id',
-		'desc' 			=> esc_html__( 'Just the ID number from the FB video URL', 'cmb2' ),
+	$metabox_prefix->add_field( array(
+		'name'			=> esc_html__( 'Label Name', 'cmb2' ),
+		'id'    		=> $prefix . 'input_id',
+		'desc' 			=> esc_html__( 'Optional description text.', 'cmb2' ),
 		'type'  		=> 'sidekick_text',
 		'icon_block'		=> 'left',  // options: 'left', 'right'
-		'fa_icon'			=> 'fal fa-hashtag',  // full fontawesome markup: 'fal fa-truck'
+		'fa_icon'			=> 'fal fa-paper-plane',  // full fontawesome markup: 'fal fa-truck'
 		'icon_color'		=> 'text-gray-darken-2',  // Use BS4 "color-palette" full class 'text-dark-lighten-2'
 		'icon_bg'			=> 'bg-gray-lighten-6',  // Use BS4 "color-palette" full class 'bg-dark-lighten-4'
 		'icon_border'		=> 'no',  // Default is 'yes' or true. Option: 'no'.
 		'classes'		=> array( 'col-12', 'col-md-4', 'col-lg-3' ),
 	) );
 
-	$cn_sermon_meta->add_field( array(
-		'name'			=> esc_html__( 'Scripture References', 'cmb2' ),
-		'id'    		=> $prefix . 'scripture_ref',
-		'desc' 			=> esc_html__( 'Separate references with a comma', 'cmb2' ),
-		'type'  		=> 'sidekick_text',
-		'icon_block'		=> 'left',  // options: 'left', 'right'
-		'fa_icon'			=> 'fal fa-bible',  // full fontawesome markup: 'fal fa-truck'
-		'icon_color'		=> 'text-gray-darken-2',  // Use BS4 "color-palette" full class 'text-dark-lighten-2'
-		'icon_bg'			=> 'bg-gray-lighten-6',  // Use BS4 "color-palette" full class 'bg-dark-lighten-4'
-		'icon_border'		=> 'no',  // Default is 'yes' or true. Option: 'no'.
-		'classes'		=> array( 'col-12', 'col-md-4', 'col-lg-3' ),
-	) );
-
-	$cn_sermon_meta->add_field( array(
-		'name'			=> esc_html__( 'Sermon Outline PDF', 'cmb2' ),
-		'id'    		=> $prefix . 'outline',
-		'desc' 			=> esc_html__( 'Insert URL or upload', 'cmb2' ),
-		'type'  		=> 'sidekick_file',
-		'options' 		=> array(
-	        'url' => true, 
-	    ),
-	    'text'    		=> array(
-	        'add_upload_file_text' => 'Upload' // Change upload button text. Default: "Add or Upload File"
-	    ),
-		'classes'		=> array( 'col-12', 'col-md-4', 'col-lg-3' ),
-	) );
-
-	$cn_sermon_meta->add_field( array(
-		'name'			=> esc_html__( 'Audio File', 'cmb2' ),
-		'id'    		=> $prefix . 'audio',
-		'desc' 			=> esc_html__( 'Insert URL or upload', 'cmb2' ),
-		'type'  		=> 'sidekick_file',
-		'options' 		=> array(
-	        'url' => true, 
-	    ),
-	    'text'    		=> array(
-	        'add_upload_file_text' => 'Upload' // Change upload button text. Default: "Add or Upload File"
-	    ),
-		'classes'		=> array( 'col-12', 'col-md-4', 'col-lg-3' ),
-	) );
-
-	$cn_sermon_meta->add_field( array(
-		'name'			=> esc_html__( 'Video File', 'cmb2' ),
-		'id'    		=> $prefix . 'video',
-		'desc' 			=> esc_html__( 'Insert URL or upload', 'cmb2' ),
-		'type'  		=> 'sidekick_file',
-		'options' 		=> array(
-	        'url' => true, 
-	    ),
-	    'text'    		=> array(
-	        'add_upload_file_text' => 'Upload' // Change upload button text. Default: "Add or Upload File"
-	    ),
-		'classes'		=> array( 'col-12', 'col-md-4', 'col-lg-3' ),
-	) );
-
-	$cn_sermon_meta->add_field( array(
-		'name'			=> esc_html__( 'Youtube Video ID', 'cmb2' ),
-		'id'    		=> $prefix . 'youtube',
-		'desc' 			=> esc_html__( 'NOT full URL. Just the video ID.', 'cmb2' ),
-		'type'  		=> 'sidekick_text',
-		'icon_block'		=> 'left',  // options: 'left', 'right'
-		'fa_icon'			=> 'fab fa-youtube',  // full fontawesome markup: 'fal fa-truck'
-		'icon_color'		=> 'text-gray-darken-2',  // Use BS4 "color-palette" full class 'text-dark-lighten-2'
-		'icon_bg'			=> 'bg-gray-lighten-6',  // Use BS4 "color-palette" full class 'bg-dark-lighten-4'
-		'icon_border'		=> 'no',  // Default is 'yes' or true. Option: 'no'.
-		'classes'		=> array( 'col-12', 'col-md-4', 'col-lg-3' ),
-	) );
-
-	$cn_sermon_meta->add_field( array(
+	$metabox_prefix->add_field( array(
 	    'id'				=> $hidden . 'end',
 	    'type'				=> 'title',
 	    'after_row'		=> 
