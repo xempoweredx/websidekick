23850319a77e614ffff633db9e17c549d15eaac9
xempoweredx <chadgray1@gmail.com>
2019-06-27 20:18:13 -0500

    Update Tax_CPT_Template.php

--- admin/tax/Tax_CPT_Template.php
+++ admin/tax/Tax_CPT_Template.php
@@ -1,26 +1,32 @@
 <?php
 
+$singular = 'Taxonomy';
+$plural = 'Taxonomies';
+$singular_lower = 'taxonomy';
+$plural_lower = 'taxonomies';
+$cpts = array( 'cpt1', 'cpt2' );
+
 $labels = array(
-	'name'                       => _x( 'References', 'Taxonomy General Name', 'church_ninja' ),
-	'singular_name'              => _x( 'Reference', 'Taxonomy Singular Name', 'church_ninja' ),
-	'menu_name'                  => __( 'References', 'church_ninja' ),
-	'all_items'                  => __( 'All References', 'church_ninja' ),
-	'parent_item'                => __( 'Parent Reference', 'church_ninja' ),
-	'parent_item_colon'          => __( 'Parent Reference:', 'church_ninja' ),
-	'new_item_name'              => __( 'New Reference Name', 'church_ninja' ),
-	'add_new_item'               => __( 'Add New Reference', 'church_ninja' ),
-	'edit_item'                  => __( 'Edit Reference', 'church_ninja' ),
-	'update_item'                => __( 'Update Reference', 'church_ninja' ),
-	'view_item'                  => __( 'View Reference', 'church_ninja' ),
-	'separate_items_with_commas' => __( 'Separate References with commas', 'church_ninja' ),
-	'add_or_remove_items'        => __( 'Add or remove References', 'church_ninja' ),
-	'choose_from_most_used'      => __( 'Choose from the most used References', 'church_ninja' ),
-	'popular_items'              => __( 'Popular References', 'church_ninja' ),
-	'search_items'               => __( 'Search References', 'church_ninja' ),
-	'not_found'                  => __( 'Not Found', 'church_ninja' ),
-	'no_terms'                   => __( 'No References', 'church_ninja' ),
-	'items_list'                 => __( 'References list', 'church_ninja' ),
-	'items_list_navigation'      => __( 'References list navigation', 'church_ninja' ),
+	'name'                       => _x( $plural, 'Taxonomy General Name', 'websidekick' ),
+	'singular_name'              => _x( $singular, 'Taxonomy Singular Name', 'websidekick' ),
+	'menu_name'                  => __( $plural, 'websidekick' ),
+	'all_items'                  => __( 'All ' . $plural, 'websidekick' ),
+	'parent_item'                => __( 'Parent ' . $singular, 'websidekick' ),
+	'parent_item_colon'          => __( 'Parent ' . $singular ':', 'websidekick' ),
+	'new_item_name'              => __( 'New ' . $singular . ' Name', 'websidekick' ),
+	'add_new_item'               => __( 'Add New ' . $singular, 'websidekick' ),
+	'edit_item'                  => __( 'Edit ' . $singular, 'websidekick' ),
+	'update_item'                => __( 'Update ' . $singular, 'websidekick' ),
+	'view_item'                  => __( 'View ' . $singular, 'websidekick' ),
+	'separate_items_with_commas' => __( 'Separate ' . $plural . ' with commas', 'websidekick' ),
+	'add_or_remove_items'        => __( 'Add or remove ' . $plural, 'websidekick' ),
+	'choose_from_most_used'      => __( 'Choose from the most used ' . $plural, 'websidekick' ),
+	'popular_items'              => __( 'Popular ' . $plural, 'websidekick' ),
+	'search_items'               => __( 'Search ' . $plural, 'websidekick' ),
+	'not_found'                  => __( 'Not Found', 'websidekick' ),
+	'no_terms'                   => __( 'No ' . $plural, 'websidekick' ),
+	'items_list'                 => __( $plural . ' list', 'websidekick' ),
+	'items_list_navigation'      => __( $plural . ' list navigation', 'websidekick' ),
 );
 $args = array(
 	'labels'                     => $labels,
@@ -32,5 +38,5 @@ $args = array(
 	'show_tagcloud'              => true,
 	'show_in_rest'               => true,
 );
-register_taxonomy( 'references', array( 'resources', 'sermons', 'post' ), $args );
+register_taxonomy( $plural, $cpts, $args );
 	
