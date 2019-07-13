74b9f2f1d24b67c4ae248bef0d8c47031d39dbad
xempoweredx <chadgray1@gmail.com>
2019-07-13 15:48:14 -0500

    Remove shadow tax

--- admin/partials/options-main.php
+++ admin/partials/options-main.php
@@ -140,19 +140,6 @@
 		) );
 
 		$main_options->add_field( array(
-			'name' => 'Enable',
-			'id'   => 'shadow_tax',
-			'type' => 'sidekick_switch_toggle',
-			'before' => '<div class="mb-2">Shadow Taxonomy</div>',
-			'desc' => 'A shadow taxonomy is a custom WordPress taxonomy which is created to mirror a specific post type. So anytime a post in that post type is created, updated, or deleted, the associated shadow taxonomy term is also created, updated, and deleted.',
-			'color_unselected'		=> 'text-gray-lighten-7',
-		    'color_selected'	=> 'text-gray-lighten-7',
-		    'bg_unselected'		=> 'bg-gray-lighten-4',
-		    'bg_selected'		=> 'bg-green-lighten-2',
-			'classes' => array( 'col-12' ),
-		) );
-
-		$main_options->add_field( array(
 			//'name'    => __( 'Core Files Settings', 'websidekick' ),
 			//'desc'		=> __( 'Default - "off" Core files are ONLY loaded on post & taxonomy edit screens. Turning this option on would load all files globally and change the look of the WP Admin Dashboard.', 'websidekick' ),
 			'id'      => 'core_title',
