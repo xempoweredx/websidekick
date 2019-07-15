475b18b709e6eee83ca24f1e09c1922f14678d95
xempoweredx <chadgray1@gmail.com>
2019-07-15 14:33:47 -0500

    Update websidekick-global.js

--- includes/app_js/websidekick-global.js
+++ includes/app_js/websidekick-global.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function( $ ) {
 	$('[data-toggle="popover"]').popover()
 
 	// Prevent empty links which use jQuery from jumping to top of page
-    $('.ninjaMetabox a.empty-link').click(function(e)
+    $('.sidekickMetabox a.empty-link').click(function(e)
 		{ e.preventDefault(); 
 	});
 
@@ -39,23 +39,27 @@ jQuery(document).ready(function( $ ) {
 	};
 
 	// Input Mask - Phone Inputs
-	$('.cmb-type-ninja-tel input').mask('(000) 000-0000', {placeholder: "(___) ___-____"});
+	$('.sidekickMetabox .cmb-type-sidekick-tel input').mask('(000) 000-0000', {placeholder: "(___) ___-____"});
 
-	$( '.ninjaMetabox table:not(.table)' ).addClass( 'table' );
-	$( '.cmb-multicheck-toggle' ).addClass( 'btn rounded-10 btn-sm btn-outline-secondary' ).removeClass( 'button-secondary' );
+	$( '.sidekickMetabox table:not(.table)' ).addClass( 'table' );
+	$( '.sidekickMetabox .cmb-multicheck-toggle' ).addClass( 'btn rounded-10 btn-sm btn-outline-secondary' ).removeClass( 'button-secondary' );
 
 		/** Add active class to selected option **/
-	$( '.radio-buttons-list.cmb2-list label.btn > input:checked' ).parent().addClass( 'active' );
-	$( '.cmb-type-ninja-switch-buttons .cmb2-list label.btn > input:checked' ).parent().addClass( 'active' );
+	$( '.sidekickMetabox .radio-buttons-list.cmb2-list label.btn > input:checked' ).parent().addClass( 'active' );
+	$( '.sidekickMetabox .cmb-type-sidekick-switch-buttons .cmb2-list label.btn > input:checked' ).parent().addClass( 'active' );
 
-	$( '.ninjaMetabox textarea:not(.form-control)' ).addClass( 'form-control' );
-	$( '.ninjaMetabox select:not(.custom-select)' ).addClass( 'custom-select' );
-	$( '.cmb-type-ninja-file .cmb2-upload-file:not(.form-control)' ).addClass( 'form-control' );
+	$( '.sidekickMetabox textarea:not(.form-control)' ).addClass( 'form-control' );
+	$( '.sidekickMetabox select:not(.custom-select)' ).addClass( 'custom-select' );
+	$( '.sidekickMetabox .cmb-type-sidekick-file .cmb2-upload-file:not(.form-control)' ).addClass( 'form-control' );
 	
-	//$( '.ninjaMetabox .cmb-type-ninja-file input:not(.custom-file-input)' ).addClass( 'custom-file-input' );
-	//$( '.ninjaMetabox .cmb-type-ninja-file label:not(.custom-file-labe)' ).addClass( 'custom-file-labe' );
+	//$( '.ninjaMetabox .cmb-type-sidekick-file input:not(.custom-file-input)' ).addClass( 'custom-file-input' );
+	//$( '.ninjaMetabox .cmb-type-sidekick-file label:not(.custom-file-labe)' ).addClass( 'custom-file-labe' );
 
-	$( '.ninjaMetabox .cmb-type-ninja-text-datetime-timestamp-timezone .cmb2-datepicker' ).addClass( 'form-control ninja-datepicker' ).removeClass( 'cmb2-text-small' );
-	$( '.ninjaMetabox .cmb-type-ninja-text-datetime-timestamp-timezone .cmb2-timepicker' ).addClass( 'form-control ninja-timepicker' ).removeClass( 'cmb2-text-small' );
+	$( '.sidekickMetabox .cmb-type-sidekick-text-datetime-timestamp-timezone .cmb2-datepicker' ).addClass( 'form-control sidekick-datepicker' ).removeClass( 'cmb2-text-small' );
+	$( '.sidekickMetabox .cmb-type-sidekick-text-datetime-timestamp-timezone .cmb2-timepicker' ).addClass( 'form-control sidekick-timepicker' ).removeClass( 'cmb2-text-small' );
+
+	$( '.cmb-type-term-select input[type="text"]' ).addClass( 'form-control' );
+
+	//$( '.sidekickMetabox .postbox.cmb-row.cmb-repeatable-grouping' ).addClass( 'd-block' );
 
 });
